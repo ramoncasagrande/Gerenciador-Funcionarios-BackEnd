9a34565003696f25f46c883f9e1b832189c40b52
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-01-25 15:09:17 -0300

    Criada Classe Funcionario

---
+++ src/main/java/com/ramon/gerenciador/model/Funcionario.java
@@ -0,0 +1,23 @@
+package com.ramon.gerenciador.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Funcionario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String cargo;
+    private String telefone;
+    private String imagemUrl;
+    private String codigoRegistro;
+    
+}
