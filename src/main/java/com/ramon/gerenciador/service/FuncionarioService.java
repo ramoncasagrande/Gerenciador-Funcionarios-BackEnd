98c702266df16c4b2f2a52e584a683d404e52489
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-01-26 09:35:20 -0300

    Criado FuncionarioService

---
+++ src/main/java/com/ramon/gerenciador/service/FuncionarioService.java
@@ -0,0 +1,44 @@
+package com.ramon.gerenciador.service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ramon.gerenciador.model.Funcionario;
+import com.ramon.gerenciador.repository.FuncionarioRepository;
+
+@Service
+public class FuncionarioService {
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+    public List<Funcionario> listar(){
+        List<Funcionario> funcionarios = funcionarioRepository.findAll();
+        return funcionarios.stream()
+                .collect(Collectors.toList());
+    }
+
+    public Funcionario adicionar(Funcionario funcionario){
+        funcionario.setCodigoRegistro(UUID.randomUUID().toString());
+        return funcionarioRepository.save(funcionario);
+    }
+
+    public Funcionario atualizar(Funcionario funcionario){
+        return funcionarioRepository.save(funcionario);
+    }
+
+    public Funcionario buscar(Long id){
+        return funcionarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+    }
+
+    public void excluir(Long id){
+        funcionarioRepository.deleteById(id);
+    }
+    
+    
+}
